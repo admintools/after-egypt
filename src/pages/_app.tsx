a5eb4c1a5281b7fa6e62c2522688e288b043b720
Cristian O <53712246+admintools@users.noreply.github.com>
2023-04-21 14:42:28 +0300

    .gitignore is now working

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import { AppProps } from "next/app";
 import { appWithTranslation } from "next-i18next";
 import "../style/globals.css";
+import Favicon from "../components/Favicon/Favicon";
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Favicon />
+      <Component {...pageProps} />
+    </>
+  );
+}
 
 export default appWithTranslation(MyApp);
